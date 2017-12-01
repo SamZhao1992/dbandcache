753f86311fc4d46027d1d3aed6c3f4bce4facd9a
zhixin.zhao <064868@600869.com>
2017-12-01 16:58:52 +0800

    add

---
+++ db/car.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Enterprise v12.09 (64 bit)
+MySQL - 5.7.17-log : Database - microservicesdb2
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`microservicesdb2` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `microservicesdb2`;
+
+/*Table structure for table `t_car` */
+
+DROP TABLE IF EXISTS `t_car`;
+
+CREATE TABLE `t_car` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL COMMENT '车名',
+  `color` varchar(255) DEFAULT NULL COMMENT '车的颜色',
+  `owner` bigint(20) DEFAULT NULL COMMENT '车主',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_car` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
